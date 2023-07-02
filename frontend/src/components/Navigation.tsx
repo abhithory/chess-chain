8da9903a346b2d294a6a46b5d807d40776c42402
stoic <abhishekthory001@gmail.com>
2023-07-02 11:13:08 +0530

    navigation style added

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -2,11 +2,22 @@
 
 import React from 'react'
 import { ConnectWallet } from "@thirdweb-dev/react";
+import Image from 'next/image'
 
 
 function Navigation() {
   return (
-    <div className="w-screen flex_center">
+    <div className="glassmorphism-bg bg-secondary/30 w-full flex justify-between px-20 py-4">
+    <span className="flex items-center justify-center gap-4">
+    <Image
+      src="/assests/logo.png"
+      width={40}
+      height={40}
+      alt="Picture of the author"
+      />
+      <h1 className="text-2xl text-text-color ">Chess Chain</h1>
+      </span>
+
       <ConnectWallet />
     </div>
   )
