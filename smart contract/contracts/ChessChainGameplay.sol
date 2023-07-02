c1ac17c9a1319464e1ada79e43536a6e6e9267bc
abhithory <abhithory@gmail.com>
2023-07-03 00:44:16 +0530

    smart contract improved

--- smart contract/contracts/ChessChainGameplay.sol
+++ smart contract/contracts/ChessChainGameplay.sol
@@ -152,7 +152,7 @@ contract ChessChainGameplay is Ownable,ERC721, ReentrancyGuard, ERC721URIStorage
     }
 
 
-    // The following functions are overrides required by Solidity.
+    // The following functions are overrides required by Solidity for nft contract.
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
         override(ERC721, ERC721Enumerable)
